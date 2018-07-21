21fcf83b7138c156ec9c5e48e9d9f88f5251d12f
Abdulla Mohamed <abdll1@hotmail.com>
2018-07-21 18:34:57 +0100

    create visualization class

--- src/visual_attention_base/scripts/markerClass.py
+++ src/visual_attention_base/scripts/markerClass.py
@@ -17,57 +17,58 @@ for i in range(0,5):
     probability3D = np.random.randint(0.0,100.0,(1)).tolist()
     dummyList.append([probability2D, probability3D, pose2D, pose3D])
 
+class CreateVisualizationMarker:
 
-# print (dummyList[0][0])
-markerArray = MarkerArray()
+    def __init__(self, frame):
+        self.marker_publisher = rospy.Publisher('visualization_cognitive_map', MarkerArray, queue_size=5)
+        self.frame = frame
+        self.dummyCount
 
-def createMarker2(idea, text, position):
-    marker_text = Marker()
-    marker_text.header.frame_id = "map"
-    marker_text.type = Marker.TEXT_VIEW_FACING
-    marker_text.action = Marker.ADD
-    marker_text.scale.x = 0.2
-    marker_text.scale.y = 0.2
-    marker_text.scale.z = 0.2
-    marker_text.color.a = 1.0
-    marker_text.color.r = 1.0
-    marker_text.color.g = 1.0
-    marker_text.color.b = 0.0
-    marker_text.pose.orientation.w = 1.0
-    marker_text.pose.position.x = position[0]
-    marker_text.pose.position.y = position[1]
-    marker_text.pose.position.z = position[2]
-    marker_text.id = idea
-    marker_text.text = text
-    return marker_text
+        
+    def createMarker(self, idea, text, position):
+        marker_text = Marker()
+        marker_text.header.frame_id = self.frame
+        marker_text.type = Marker.TEXT_VIEW_FACING
+        marker_text.action = Marker.ADD
+        marker_text.scale.x = 0.2
+        marker_text.scale.y = 0.2
+        marker_text.scale.z = 0.2
+        marker_text.color.a = 1.0
+        marker_text.color.r = 1.0
+        marker_text.color.g = 1.0
+        marker_text.color.b = 0.0
+        marker_text.pose.orientation.w = 1.0
+        marker_text.pose.position.x = position[0]
+        marker_text.pose.position.y = position[1]
+        marker_text.pose.position.z = position[2]
+        marker_text.id = idea
+        marker_text.text = text
+        return marker_text
 
-def publishMarkerArray(marker_publisher, List):
-    markerArray = MarkerArray()
-    for i, list in enumerate(List):
-        text = convertInformationToString(i, list[0][0], list[1][0],list[2], list[3])
-        print( text)
-        marker = createMarker2(i, text, list[3])
-        markerArray.markers.append(marker)
+    def publishMarkerArray(self, List):
+        markerArray = MarkerArray()
+        for i, list in enumerate(List):
+            text = self.convertInformationToString(i, list[0][0], list[1][0],list[2], list[3])
+            marker = self.createMarker(i, text, list[3])
+            markerArray.markers.append(marker)
+            # self.dummyCount += 1
+        self.marker_publisher.publish(markerArray)
 
-    marker_publisher.publish(markerArray)
 
-
-def convertInformationToString(idea, probability2D, probability3D, pose2D, pose3D):
-    string = 'Tomato ID: ' + str(idea)
-    string = string+ '\n2D propability: ' + str(probability2D) + '%'
-    string = string + '\n2D pose: ' + str(pose2D)
-    string = string + '\n3D propability: ' + str(probability3D) + '%'
-    string = string + '\n3D pose: ' + str(pose3D)
-
-    return string
+    def convertInformationToString(self, idea, probability2D, probability3D, pose2D, pose3D):
+        string = 'Tomato ID: ' + str(idea)
+        string = string+ '\n2D propability: ' + str(probability2D) + '%'
+        string = string + '\n2D pose: ' + str(pose2D)
+        string = string + '\n3D propability: ' + str(probability3D) + '%'
+        string = string + '\n3D pose: ' + str(pose3D)
+        return string
 
 
 def main():
-  rospy.init_node('my_node')
-  marker_publisher = rospy.Publisher('visualization_marker', MarkerArray, queue_size=5)
-  rospy.sleep(0.5)
+  rospy.init_node('visualAttention_information')
+  createVisualizationMarker = CreateVisualizationMarker('map')
   while not rospy.is_shutdown():
-      publishMarkerArray(marker_publisher, dummyList)
+      createVisualizationMarker.publishMarkerArray(dummyList)
 
 
 if __name__ == '__main__':
